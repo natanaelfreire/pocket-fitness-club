5ae385ab77e371bf860bc28fa12bda4386ed1f77
natanaelfreire <natan.nascimento.freire@gmail.com>
2022-07-02 19:47:15 -0300

    módulo colaborador

---
+++ server/src/controllers/ColaboradorController.ts
@@ -0,0 +1,144 @@
+import { Request, Response } from 'express';
+import { prisma } from "../prisma";
+
+interface ColaboradorDadosCriacao {
+  nome: string;
+  cpf: string;
+  endereco: string;
+  telefone: string;
+  especialidade: string;
+  formacao: string;
+  cargo: string;
+}
+
+export class ColaboradorController {
+  async criar(req: Request, res: Response) {
+    try {
+      const {
+        nome,
+        cpf,
+        endereco,
+        telefone,
+        especialidade,
+        formacao,
+        cargo
+      } : ColaboradorDadosCriacao = req.body;
+
+      if (!nome) {
+        throw new Error("Nome é obrigatório.");
+      }
+
+      if (!cpf) {
+        throw new Error("CPF é obrigatório.");
+      }
+
+      if (!cargo) {
+        throw new Error("Cargo é obrigatório.");
+      }
+
+      const cpfSemFormatacao = cpf.replace(/\./g, "").replace("-", "").trim()
+      const telefoneSemFormatacao = telefone.replace("(", "").replace(")", "").replace("-", "").replace(/\s/g, "").trim()
+
+      await prisma.colaborador.create({
+        data: {
+          nome: nome.toUpperCase(),
+          cpf: cpfSemFormatacao,
+          telefone: telefoneSemFormatacao,
+          endereco,
+          especialidade,
+          formacao,
+          cargo,
+        }
+      })
+
+      return res.status(201).send();
+      
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }   
+  }
+
+  async listar(req: Request, res: Response) {
+    try {
+      const colaboradores = await prisma.colaborador.findMany({
+        orderBy: {
+          nome: 'asc'
+        },
+      });
+
+      return res.status(200).json(colaboradores);
+      
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
+
+  async atualizar(req: Request, res: Response) {
+    try {
+      const {
+        id,
+        nome,
+        cpf,
+        endereco,
+        telefone,
+        especialidade,
+        formacao,
+        cargo,
+      } = req.body;
+
+      await prisma.colaborador.update({
+        where: {
+          id: id
+        },
+        data: {
+          nome,
+          cpf,
+          endereco,
+          telefone,
+          especialidade,
+          formacao,
+          cargo,
+        }
+      })
+
+      return res.status(200).send('Colaborador atualizado com sucesso!');
+      
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
+
+  async deletar(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      await prisma.colaborador.delete({
+        where: {
+          id: id
+        }
+      })
+
+      return res.status(200).send('Colaborador deletado com sucesso!');
+      
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
+
+  async mostrar(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const colaborador = await prisma.colaborador.findFirst({
+        where: {
+          id: id
+        }
+      })
+
+      return res.status(200).json(colaborador);
+      
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import { ClienteController } from './controllers/ClienteController';
+import { ColaboradorController } from './controllers/ColaboradorController';
 
 const routes = express.Router();
 const clienteController = new ClienteController();
+const colaboradorController = new ColaboradorController();
 
 routes.post('/cliente', clienteController.criar);
 routes.post('/cliente/listagem', clienteController.listar);
 routes.put('/cliente', clienteController.atualizar);
 routes.get('/cliente/:id', clienteController.mostrar);
 routes.delete('/cliente/:id', clienteController.deletar);
+
+routes.post('/colaborador', colaboradorController.criar);
+routes.get('/colaborador', colaboradorController.listar);
+routes.put('/colaborador', colaboradorController.atualizar);
+routes.get('/colaborador/:id', colaboradorController.mostrar);
+routes.delete('/colaborador/:id', colaboradorController.deletar);
 
 routes.get('/', (req, res) => {
   return res.send("Eae");
